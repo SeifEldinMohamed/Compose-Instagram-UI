48a32404f5914630f43f6fb5104a6048af60abd7
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-09-18 22:12:41 +0200

    Setup Project

---
+++ app/src/main/java/com/seif/composeinstagramui/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.seif.composeinstagramui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.seif.composeinstagramui.ui.theme.ComposeInstagramUiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeInstagramUiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeInstagramUiTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                Greeting("Seif")
+            }
+        }
+    }
+}
