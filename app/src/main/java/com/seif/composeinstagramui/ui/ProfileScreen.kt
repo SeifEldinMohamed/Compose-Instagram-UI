9d0b80a7910d85555051c762d8fbd910bd784824
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-09-19 13:24:16 +0200

    HighLights Section

--- app/src/main/java/com/seif/composeinstagramui/ui/ProfileScreen.kt
+++ app/src/main/java/com/seif/composeinstagramui/ui/ProfileScreen.kt
@@ -1,18 +1,20 @@
 package com.seif.composeinstagramui.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.seif.composeinstagramui.*
-import com.seif.composeinstagramui.ui.components.ButtonsSection
-import com.seif.composeinstagramui.ui.components.ProfileDetailsSection
-import com.seif.composeinstagramui.ui.components.StatDescriptionSection
-import com.seif.composeinstagramui.ui.components.TopBarSection
+import com.seif.composeinstagramui.R
+import com.seif.composeinstagramui.ui.components.*
 
 @Composable
 fun ProfileScreen(
@@ -33,9 +35,59 @@ fun ProfileScreen(
         StatDescriptionSection(statDescription)
         Spacer(modifier = Modifier.height(16.dp))
         ButtonsSection()
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(12.dp))
+        HighLightsSection(createHighLightList(), modifier.padding(start = 10.dp))
     }
 
 }
 
+fun createHighLightList(): List<HighLight> {
+    return listOf(
+        HighLight(R.drawable.zamalek, "Zamalek ❤"),
+        HighLight(R.drawable.zamalek_shirt, "Shirt 2022"),
+        HighLight(R.drawable.shika, "Legend 😎"),
+        HighLight(R.drawable.seif_ecpc, "ECPC"),
+        HighLight(R.drawable.seif_crop, "raye2")
+    )
+}
 
+@Composable
+fun HighLightsSection(
+    highlights: List<HighLight>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(modifier = modifier
+        .fillMaxWidth()
+        .padding(end = 12.dp)) {
+        items(highlights.size) {
+            HighLightItem(highlights[it])
+        }
+    }
+}
+
+@Composable
+fun HighLightItem(highLight: HighLight, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(end = 16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        RoundImage(
+            userImage = painterResource(id = highLight.image),
+            modifier = modifier.size(70.dp)
+        )
+        Text(
+            text = highLight.name,
+            modifier = Modifier.padding(top = 4.dp),
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
+data class HighLight(
+    val image: Int,
+    val name: String
+)
